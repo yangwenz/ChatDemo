29cb7a41bdba8829a249cf51537ddb5ba5b911fc
yangwenz <yangwenzhuo08@gmail.com>
2023-01-17 22:32:19 +0800

    First commit

---
+++ kubernetes/test/delete.sh
@@ -0,0 +1,8 @@
+kubectl delete deployment chat-agent
+kubectl delete deployment chat-backend
+kubectl delete deployment chat-web
+kubectl delete deployment redis-master
+
+kubectl delete service chat-backend
+kubectl delete service chat-web
+kubectl delete service redis-master
