00cd4adb8b730d56a861c6c70e85754ef20c6c74
yangwenz <yangwenzhuo08@gmail.com>
2023-01-18 12:40:24 +0800

    First commit

--- build_gcp_images.sh
+++ build_gcp_images.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-TAG="gcr.io/salesforce-research-internal/chatdemo"
-gcloud builds submit . -t=$TAG --machine-type=n1-highcpu-32 --timeout=9000
+# gcloud builds submit . -t=$TAG --machine-type=n1-highcpu-32 --timeout=9000
+
+gcloud builds submit . \
+--config=./cloudbuild/backend.yaml \
+--substitutions=_IMAGE_NAME="chatdemo-backend",_STAGE="v1" \
+--timeout=9000
+
+gcloud builds submit . \
+--config=./cloudbuild/web.yaml \
+--substitutions=_IMAGE_NAME="chatdemo-web",_STAGE="v1" \
+--timeout=9000
+
+gcloud builds submit . \
+--config=./cloudbuild/agent.yaml \
+--substitutions=_IMAGE_NAME="chatdemo-agent",_STAGE="v1" \
+--machine-type=n1-highcpu-32  \
+--timeout=9000
