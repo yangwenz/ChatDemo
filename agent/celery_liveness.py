a56b11ff1be6ddb26f252b3effd403342b898e52
yangwenzhuo08 <wenzhuo.yang@salesforce.com>
2023-01-26 17:51:29 +0800

    First commit

--- agent/celery_liveness.py
+++ agent/celery_liveness.py
@@ -11,7 +11,7 @@ if not LIVENESS_FILE.is_file():
 stats = LIVENESS_FILE.stat()
 current_time = time.time()
 time_diff = current_time - stats.st_mtime
-if time_diff > 60:
+if time_diff > 120:
     print(f"Liveness file timestamp does not matches the given constraint: "
           f"({current_time}, {stats.st_mtime}, {time_diff}).")
     sys.exit(1)
