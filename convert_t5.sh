3c541a290754c5f42905bd54d60d3f76681e3db7
yangwenz <yangwenzhuo08@gmail.com>
2023-02-14 20:19:54 +0800

    First commit

--- convert_t5.sh
+++ convert_t5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MODEL_DIR=/export/share/wenzhuo/GPT-JT-6B-v1
-OUTPUT_DIR=/export/share/wenzhuo/
+MODEL_DIR=/home/ywz/data/models/flan-t5-large
+OUTPUT_DIR=/home/ywz/data/models/
 NUM_GPU=1
 
 python ./utils/t5/generate_triton_config.py --model_store ${OUTPUT_DIR} --hf_model_dir ${MODEL_DIR} --num_gpu ${NUM_GPU}
-python ./utils/t5/huggingface_t5_ckpt_convert.py -o ${OUTPUT_DIR} -i ${MODEL_DIR} -i_g ${NUM_GPU}
+python ./utils/t5/huggingface_t5_ckpt_convert.py -o ${OUTPUT_DIR} -i ${MODEL_DIR} -i_g ${NUM_GPU} -weight_data_type fp32
