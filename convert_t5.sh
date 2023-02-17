65f97794ca6c7f3e6269e1ff7828e5f451b27594
yangwenz <yangwenzhuo08@gmail.com>
2023-02-17 14:09:47 +0800

    First commit

--- convert_t5.sh
+++ convert_t5.sh
@@ -5,4 +5,4 @@ OUTPUT_DIR=/home/ywz/data/models/
 NUM_GPU=1
 
 python ./utils/t5/generate_triton_config.py --model_store ${OUTPUT_DIR} --hf_model_dir ${MODEL_DIR} --num_gpu ${NUM_GPU}
-python ./utils/t5/huggingface_t5_ckpt_convert.py -o ${OUTPUT_DIR} -i ${MODEL_DIR} -i_g ${NUM_GPU} -weight_data_type fp16
+python ./utils/t5/huggingface_t5_ckpt_convert.py -o ${OUTPUT_DIR} -i ${MODEL_DIR} -i_g ${NUM_GPU} -weight_data_type fp32
